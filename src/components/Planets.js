9897d4ff9353407dfee95e658e1255c858325f7b
Mohamed Ibrahim Mohamed Yossef <49948244+MohamedIbrahim13@users.noreply.github.com>
2021-05-23 10:34:37 +0200

    Update Planets.js

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -4,7 +4,7 @@ import Planet from './Planet';
 import axios from "axios";
 
 const fetchPlanets = async(key,page)=>{
-  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
+  const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 }
 
